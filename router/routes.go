16cc4cd5e103b8642bb4e783743bd42a502c8426
unknown <peruccii2917@hotmail.com>
2024-05-09 11:30:31 -0300

    separing routes to handlers

--- router/routes.go
+++ router/routes.go
@@ -1,10 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/peruccii/gopportunities/handler"
+)
 
 func initializeRoutes(router *gin.Engine) { // Create Routes
 	v1 := router.Group("/api/v1") // Agroup Routes and define api prefix
 	{
-		v1.GET("/example")
+		v1.GET("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.CreateOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.PUT("/opening", handler.CreateOpeningHandler)
+		v1.GET("/openings", handler.CreateOpeningHandler)
 	}
 }
