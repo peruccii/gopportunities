4c0e12f07f1f3eb9028be8f657a3e15a25975fda
Eduardo Perucci <peruccii2917@hotmail.com>
2024-05-10 14:48:20 -0300

    feat: add validation params and creating opening handler

--- handler/createOpeningHandler.go
+++ handler/createOpeningHandler.go
@@ -2,11 +2,38 @@ package handler
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/peruccii/gopportunities/entity"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET OPENING", // Show in the body
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.ErrF("validantion error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return 
+	}
+
+	opening := entity.Opening{
+		Role: request.Role,
+		Company: request.Company,
+		Location: request.Location,
+		Link: request.Link,
+		Remote: *request.Remote,
+		Salary: request.Salary,
+	}
+
+	logger.InfoF("request received %+v", request)
+
+	if err := db.Create(&opening).Error; err != nil{
+		logger.ErrF("error creating opening %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opnening on database")
+		return 
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
